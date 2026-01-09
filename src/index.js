22bf831338ac27cdbee8903af73e9dcf0e15fb09
Shakir <shakir@example.com>
2026-01-09 02:25:16 -0800

    Fix React 17 compatibility - replace ReactDOM.createRoot with ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from '../src/main-component/App/App';
 import reportWebVitals from './reportWebVitals';
 import './css/font-awesome.min.css';
@@ -13,13 +13,13 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/index";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
             <App />
         </PersistGate>
-    </Provider>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
